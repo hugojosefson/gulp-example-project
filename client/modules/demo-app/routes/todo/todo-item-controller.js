7658998e044c8048f453648db08984fa0dc4c3ce
Hugo Josefson <hugo@josefson.org>
2014-09-06 20:43:15 +0200

    Use hypermedia json schema in update form.

--- client/modules/demo-app/routes/todo/todo-item-controller.js
+++ client/modules/demo-app/routes/todo/todo-item-controller.js
@@ -15,4 +15,8 @@ angular.module('demoApp').controller('TodoItemController', function (todosResour
         }
     });
 
+    this.isGreaterThan = function (a, b) {
+        return a > b;
+    };
+
 });
