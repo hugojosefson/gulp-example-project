ca5e4d2a8e791a8de0ee006aab0126622dae7298
Hugo Josefson <hugo@josefson.org>
2014-09-06 16:37:49 +0200

    Expose todos-resource in api-lab.

--- client/modules/demo-app/routes/api-lab/api-lab-controller.js
+++ client/modules/demo-app/routes/api-lab/api-lab-controller.js
@@ -1,5 +1,6 @@
-angular.module('demoApp').controller('ApiLabController', function (entryPointResource) {
+angular.module('demoApp').controller('ApiLabController', function (entryPointResource, todosResource) {
     'use strict';
 
     this.entryPointResource = entryPointResource;
+    this.todosResource = todosResource;
 });
